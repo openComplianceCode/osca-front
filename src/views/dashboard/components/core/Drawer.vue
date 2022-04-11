7fd2f5b09dd34c621058c40cead137ecaee08452
郑志鹏 <zhengzhipeng4@huawei.com>
2022-04-11 14:48:56 +0800

    bug fix

--- src/views/dashboard/components/core/Drawer.vue
+++ src/views/dashboard/components/core/Drawer.vue
@@ -120,6 +120,14 @@ export default {
 
   computed: {
     ...mapState(["barColor", "barImage"]),
+    drawer: {
+      get() {
+        return this.$store.state.drawer;
+      },
+      set(val) {
+        this.$store.commit("SET_DRAWER", val);
+      },
+    },
     computedItems() {
       return this.items.map(this.mapItem);
     },
@@ -143,4 +151,67 @@ export default {
 };
 </script>
 
-<style lang="sass"></style>
+<style lang="sass">
+@mixin rtl()
+  &.v-locale--is-rtl,
+  .v-locale--is-rtl &
+    @content
+
+@mixin ltr()
+  &.v-locale--is-ltr,
+  .v-locale--is-ltr &
+    @content
+
+#core-navigation-drawer
+  .v-list-group__header.v-list-item--active:before
+    opacity: .24
+
+  .v-list-item
+    &__icon--text,
+    &__icon:first-child
+      justify-content: center
+      text-align: center
+      width: 20px
+
+      +ltr()
+        margin-right: 24px
+        margin-left: 12px !important
+
+      +rtl()
+        margin-left: 24px
+        margin-right: 12px !important
+
+  .v-list--dense
+    .v-list-item
+      &__icon--text,
+      &__icon:first-child
+        margin-top: 10px
+
+  .v-list-group--sub-group
+    .v-list-item
+      +ltr()
+        padding-left: 8px
+
+      +rtl()
+        padding-right: 8px
+
+    .v-list-group__header
+      +ltr()
+        padding-right: 0
+
+      +rtl()
+        padding-right: 0
+
+      .v-list-item__icon--text
+        margin-top: 19px
+        order: 0
+
+      .v-list-group__header__prepend-icon
+        order: 2
+
+        +ltr()
+          margin-right: 8px
+
+        +rtl()
+          margin-left: 8px
+</style>
